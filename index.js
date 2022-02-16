96cb55197871feabe29e0d56ee2b2548b56de63f
Jed <vjeudy@protonmail.com>
2022-02-16 16:23:08 +0100

    Pro 2490 light refacto (#2)
    * removes lodash, adds eslint
    
    * adds mocha
    
    * refacto index file
    
    * refacto tests, make sure they run

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-var _ = require('lodash');
 module.exports = cacheOnDemand;
 
 function cacheOnDemand(fn, hasher) {
   if (!hasher) {
     hasher = cacheOnDemand.webHasher;
   }
-  if (typeof(hasher) !== 'function') {
+  if (typeof hasher !== 'function') {
     // Implement 'always'
-    var key = hasher;
-    hasher = function() {
-      return key;
-    };
+    const key = hasher;
+
+    hasher = () => key;
   }
-  var pending = {};
-  return function() {
-    // Get something we can slice
-    var argumentsArray = Array.prototype.slice.call(arguments);
-    var args = argumentsArray.slice(0, argumentsArray.length - 1);
-    var callback = argumentsArray[argumentsArray.length - 1];
-    var key = hasher.apply(this, args);
+  const pending = {};
+
+  return function(...args) {
+    const callback = args.pop();
+    const key = hasher.apply(this, args);
+
     if (key === false) {
       // hasher says this request can't be cached
       return fn.apply(this, arguments);
@@ -31,9 +28,9 @@ function cacheOnDemand(fn, hasher) {
     }
     // start a new pending queue
     pending[key] = [ callback ];
-    args.push(function() {
-      var i;
-      var list = pending[key];
+
+    args.push(function(...params) {
+      const list = pending[key];
       // Delete the queue before invoking the callbacks,
       // so we don't risk establishing a chain with no
       // breaks to generate more up-to-date results. The
@@ -42,20 +39,16 @@ function cacheOnDemand(fn, hasher) {
       // and then be open to generating a new result
       delete pending[key];
 
-      // Capture arguments being passed to callback so we
-      // can pass them to deliver()
-      var argumentsArray = Array.prototype.slice.call(arguments);
-
       // Deliver results to everyone in the queue
-      for (i = 0; (i < list.length); i++) {
-        deliver(i);
+      for (const func of list) {
+        deliver(func);
       }
 
-      function deliver(i) {
+      function deliver(func) {
         // Make sure we're async as the
         // caller expects
-        setImmediate(function() {
-          list[i].apply(this, argumentsArray);
+        setImmediate(() => {
+          func.apply(this, params);
         });
       }
     });
--- test/test.js
+++ test/test.js
@@ -1,31 +1,42 @@
-var assert = require("assert");
-var cacheOnDemand = require('../index.js');
+const assert = require('assert');
+const cacheOnDemand = require('../index.js');
 
-describe('cacheOnDemand', function(){
+describe('cacheOnDemand', () => {
+  let didTheWork = 0;
+  let fn;
 
-  var didTheWork = 0;
-  var fn = cacheOnDemand(function(a, b, callback) {
-    // Add two numbers, but take 20 ms to do it asynchronously
-    setTimeout(function() {
-      didTheWork++;
-      return callback(a + b);
-    }, 20);
-  }, function(a, b) {
-    // hash them by concatenating them
-    return a + ',' + b;
+  before(() => {
+    fn = cacheOnDemand(
+
+      (a, b, callback) => {
+      // Add two numbers, but take 20 ms to do it asynchronously
+        setTimeout(() => {
+          didTheWork++;
+
+          const result = a + b;
+          return callback(result);
+        }, 20);
+      },
+      (a, b) => {
+      // hash them by concatenating them
+        return a + ',' + b;
+      });
+
   });
 
-  it('returns functions', function() {
-    assert(fn);
+  it('returns functions', () => {
+    assert(typeof fn === 'function');
   });
-  it('delivers result ten times for ten invocations', function(done) {
-    var i;
-    var received = 0;
-    for (i = 0; (i < 10); i++) {
+
+  it('delivers result ten times for ten invocations', (done) => {
+    let received = 0;
+
+    for (let i = 0; (i < 10); i++) {
       test();
     }
+
     function test() {
-      return fn(5, 5, function(result) {
+      return fn(5, 5, (result) => {
         assert(result === 10);
         received++;
         if (received === 10) {
@@ -34,17 +45,18 @@ describe('cacheOnDemand', function(){
       });
     }
   });
-  it('does the work only once for those ten invocations', function() {
+  it('does the work only once for those ten invocations', () => {
     assert(didTheWork === 1);
   });
-  it('does the work for a second series of invocations', function(done) {
-    var i;
-    var received = 0;
-    for (i = 0; (i < 10); i++) {
+  it('does the work for a second series of invocations', (done) => {
+    let received = 0;
+
+    for (let i = 0; (i < 10); i++) {
       test();
     }
+
     function test() {
-      return fn(6, 6, function(result) {
+      return fn(6, 6, (result) => {
         assert(result === 12);
         received++;
         if (received === 10) {
@@ -53,17 +65,20 @@ describe('cacheOnDemand', function(){
       });
     }
   });
-  it('now a total of two times work has been done', function() {
+
+  it('now a total of two times work has been done', () => {
     assert(didTheWork === 2);
   });
-  it('new block of simultaneous requests generates new data, but only once', function(done) {
-    var i;
-    var received = 0;
-    for (i = 0; (i < 10); i++) {
+
+  it('new block of simultaneous requests generates new data, but only once', (done) => {
+    let received = 0;
+
+    for (let i = 0; (i < 10); i++) {
       test();
     }
+
     function test() {
-      return fn(5, 5, function(result) {
+      return fn(5, 5, (result) => {
         assert(result === 10);
         received++;
         if (received === 10) {
@@ -72,8 +87,7 @@ describe('cacheOnDemand', function(){
       });
     }
   });
-  it('now a total of 3 times work has been done', function() {
+  it('now a total of 3 times work has been done', () => {
     assert(didTheWork === 3);
   });
 });
-
